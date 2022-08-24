e7367539300195f34a39cfe2965b671013e62075
NasreddinHodja <tbizetde@gmail.com>
2022-08-24 17:16:39 -0300

    check if manga exists

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,84 @@ mod sender;
 use sender::Sender;
 
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("Rust manga sender")
         .version("1.0")
         .author("NasreddinHodja")
         .about("sends manga chapters throught sftp")
-        .arg(Arg::with_name("MANGA")
-             .help("manga folder")
-             .required(true)
-             .index(1))
-        .arg(Arg::with_name("start")
-             .short("s")
-             .value_name("START")
-             .takes_value(true)
-             .help("start manga chapter"))
-        .arg(Arg::with_name("end")
-             .short("e")
-             .value_name("END")
-             .takes_value(true)
-             .help("start manga chapter"))
+        .arg(
+            Arg::with_name("MANGA")
+                .help("manga folder")
+                // .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("start")
+                .short("s")
+                .value_name("START")
+                .takes_value(true)
+                .help("start manga chapter"),
+        )
+        .arg(
+            Arg::with_name("end")
+                .short("e")
+                .value_name("END")
+                .takes_value(true)
+                .help("end manga chapter"),
+        )
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .takes_value(false)
+                // .conflicts_with_all(&["MANGA", "start", "end"])
+                .help("configure sftp"),
+        )
         .get_matches();
 
-    let s = Sender::new();
+    let mut s = Sender::new();
 
-    let manga = matches.value_of("MANGA").unwrap();
-    if let Some(start) = matches.value_of("start") {
-        if let Some(end) = matches.value_of("end") {
-            println!("Sending {} -- chapters {} through {}",
-                    manga, start, end);
-            s.send(manga,
-                   start.parse::<u32>().unwrap(),
-                   end.parse::<u32>().unwrap())
-             .unwrap();
-        } else {
-            println!("Sending {} -- chapter {}", manga, start);
-            s.send(manga,
-                   start.parse::<u32>().unwrap(),
-                   start.parse::<u32>().unwrap())
-             .unwrap();
-        };
-    } else {
-        println!("Sending {} all chapters", manga);
-        s.send(manga, 0, 0).unwrap();
-    };
+    if matches.is_present("config") {
+        s.update_config();
+        return;
+    }
+
+    if let Some(manga) = matches.value_of("MANGA") {
+        if !s.has_manga(manga) {
+            println!("Error: {} manga was not found in source", manga);
+            return;
+        }
+
+        match matches.value_of("start") {
+            Some(start) => match matches.value_of("end") {
+                Some(end) => {
+                    println!(
+                        "Sending {} -- chapters {} through {}",
+                        manga, start, end
+                    );
+                    s.send(
+                        manga,
+                        start.parse::<u32>().unwrap(),
+                        end.parse::<u32>().unwrap(),
+                    )
+                    .unwrap();
+                }
+                _ => {
+                    println!("Sending {} -- chapter {}", manga, start);
+                    s.send(
+                        manga,
+                        start.parse::<u32>().unwrap(),
+                        start.parse::<u32>().unwrap(),
+                    )
+                    .unwrap();
+                }
+            },
+            _ => {
+                println!("Sending {} all chapters", manga);
+                s.send(manga, 0, 0).unwrap();
+            }
+        }
+    }
 }
--- src/sender.rs
+++ src/sender.rs
@@ -1,21 +1,21 @@
+use glob::glob;
+use ssh2::{Error, Session, Sftp};
+use std::fs;
 use std::io;
 use std::io::prelude::*;
-use std::io::{Read, BufReader};
-use std::fs;
+use std::io::{BufReader, Read};
 use std::net::TcpStream;
 use std::path::Path;
-use ssh2::{Session, Error, Sftp};
-use glob::glob;
 
-use serde::{Serialize, Deserialize};
 use confy;
+use rpassword;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 pub struct Sender {
     host: String,
     port: String,
     username: String,
-    password: String,
     source: String,
     destination: String,
 }
@@ -28,10 +28,14 @@ impl Sender {
             host: cfg.host,
             port: cfg.port,
             username: cfg.username,
-            password: cfg.password,
             source: cfg.source,
             destination: cfg.destination,
         }
+    }
+
+    pub fn has_manga(&mut self, manga: &str) -> bool {
+        let path = format!("{}/{}", self.source, manga);
+        Path::new(&path).exists()
     }
 
     pub fn update_config(&mut self) {
@@ -40,7 +44,6 @@ impl Sender {
         self.host = cfg.host;
         self.port = cfg.port;
         self.username = cfg.username;
-        self.password = cfg.password;
         self.source = cfg.source;
         self.destination = cfg.destination;
     }
@@ -52,8 +55,7 @@ impl Sender {
         chapter: u32,
         sftp: &Sftp,
     ) -> Result<(), Error> {
-        let path = format!("{}chapter_{:04}*",
-                            local_path, chapter);
+        let path = format!("{}chapter_{:04}*", local_path, chapter);
         let chapter_paths = glob(&path).unwrap();
 
         for chapter_path in chapter_paths {
@@ -61,29 +63,32 @@ impl Sender {
                 chapter_path.as_ref().unwrap().to_string_lossy();
             let split_path: Vec<&str> =
                 remote_chapter_path.split("/").collect();
-            let remote_chapter_path =
-                format!("{}{}/", remote_path,
-                                 &split_path[split_path.len()-1]);
-            sftp.mkdir(&Path::new(&remote_chapter_path), 0o644)
-                .expect(&format!("failed to create directory {}",
-                                 remote_chapter_path));
+            let remote_chapter_path = format!(
+                "{}{}/",
+                remote_path,
+                &split_path[split_path.len() - 1]
+            );
+            sftp.mkdir(&Path::new(&remote_chapter_path), 0o644).expect(
+                &format!("failed to create directory {}", remote_chapter_path),
+            );
 
-            println!("{}", remote_chapter_path);
             for path in fs::read_dir(chapter_path.unwrap()).unwrap() {
                 let local_path =
                     String::from(path.unwrap().path().to_string_lossy());
                 let split_path: Vec<&str> = local_path.split("/").collect();
-                let chapter_path = &split_path[split_path.len()-3..].join("/");
-                let remote_path = format!("{}{}", self.destination,
-                                          chapter_path);
+                let chapter_path =
+                    &split_path[split_path.len() - 3..].join("/");
+                let remote_path =
+                    format!("{}/{}", self.destination, chapter_path);
 
                 // read local file
                 let local_file = fs::File::open(local_path).unwrap();
                 let mut reader = BufReader::new(local_file);
                 let mut buffer = Vec::new();
-                reader.read_to_end(&mut buffer)
-                      .expect(&format!("couldn't read local {} file to buffer",
-                                      chapter_path));
+                reader.read_to_end(&mut buffer).expect(&format!(
+                    "couldn't read local {} file to buffer",
+                    chapter_path
+                ));
 
                 // write remote file
                 let mut remote_file =
@@ -107,38 +112,38 @@ impl Sender {
         let mut sess = Session::new().unwrap();
         sess.set_tcp_stream(tcp);
         sess.handshake().unwrap();
-        sess.userauth_password(&self.username, &self.password).unwrap();
+        let password = rpassword::prompt_password("password: ").unwrap();
+        sess.userauth_password(&self.username, &password).unwrap();
 
         // open sftp
         let sftp = sess.sftp().unwrap();
 
-        let local_path = format!("{}{}/",
-                                 self.source, manga_name);
-        let remote_path = format!("{}{}/",
-                                  self.destination, manga_name);
+        let local_path = format!("{}/{}/", self.source, manga_name);
+        let remote_path = format!("{}/{}/", self.destination, manga_name);
         // make remote dir for manga if non existant
         match sftp.mkdir(&Path::new(&remote_path), 0o644) {
-            Ok(_) => {},
-            Err(_) => {},
+            Ok(_) => {}
+            Err(_) => {}
         };
 
         if start_chapter == 0 && end_chapter == 0 {
             // send whole manga
             let chapters_glob = String::from(&local_path) + "chapter_*/".into();
             for chap in glob(&chapters_glob).unwrap() {
-                let chap_str = chap.unwrap()
-                    .into_os_string().into_string().unwrap();
-                let chap_str = chap_str .split("-").collect::<Vec<&str>>()[0];
-                let chap_num: u32 = chap_str .split("_").last().unwrap()
-                    .parse().unwrap();
+                let chap_str =
+                    chap.unwrap().into_os_string().into_string().unwrap();
+                let chap_str = chap_str.split("-").collect::<Vec<&str>>()[0];
+                let chap_num: u32 =
+                    chap_str.split("_").last().unwrap().parse().unwrap();
                 self.send_chapter(&local_path, &remote_path, chap_num, &sftp)
                     .unwrap()
             }
             return Ok(());
         } else {
             // send chapters
-            for i in start_chapter..(end_chapter+1) {
-                self.send_chapter(&local_path, &remote_path, i,&sftp).unwrap();
+            for i in start_chapter..(end_chapter + 1) {
+                self.send_chapter(&local_path, &remote_path, i, &sftp)
+                    .unwrap();
             }
         }
 
@@ -151,7 +156,6 @@ pub struct SenderConfig {
     host: String,
     port: String,
     username: String,
-    password: String,
     source: String,
     destination: String,
 }
@@ -167,7 +171,6 @@ impl SenderConfig {
         let mut host = String::new();
         let mut port = String::new();
         let mut username = String::new();
-        let mut password = String::new();
         let mut source = String::new();
         let mut destination = String::new();
 
@@ -179,33 +182,34 @@ impl SenderConfig {
         io::stdin()
             .read_line(&mut port)
             .expect("Error reading input");
-        println!("username");
+        println!("username:");
         io::stdin()
             .read_line(&mut username)
             .expect("Error reading input");
-        println!("password");
-        io::stdin()
-            .read_line(&mut password)
-            .expect("Error reading input");
-        println!("source");
+        println!("source:");
         io::stdin()
             .read_line(&mut source)
             .expect("Error reading input");
-        println!("destination");
+        if source.chars().last().unwrap() == '/' {
+            source = (&source[..source.len() - 1]).to_string();
+        }
+        println!("destination:");
         io::stdin()
             .read_line(&mut destination)
             .expect("Error reading input");
+        if destination.chars().last().unwrap() == '/' {
+            destination = (&destination[..destination.len() - 1]).to_string();
+        }
 
         let new_cfg = SenderConfig {
             host: host.trim().into(),
             port: port.trim().into(),
             username: username.trim().into(),
-            password: password.trim().into(),
             source: source.trim().into(),
             destination: destination.trim().into(),
         };
 
-        confy::store("rams", &new_cfg,).unwrap();
+        confy::store("rams", &new_cfg).unwrap();
         new_cfg
     }
 }
